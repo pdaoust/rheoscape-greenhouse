9a896bd8cb10c0e43656e377a214c2fa4b253495
Paul d'Aoust <amillionlobsters@gmail.com>
2024-05-06 21:00:34 -0700

    refactor timer to work better across rollovers

--- src/Timer.h
+++ src/Timer.h
@@ -71,26 +71,44 @@ class Timer : public Runnable {
     std::function<void(uint16_t)> _callback;
     bool _isComplete;
     bool _isCancelled;
-    uint16_t _count;
+    uint16_t _passedIntervals;
     std::optional<uint16_t> _times;
     bool _firstRunOnStart;
+    bool _catchUp;
 
     void _start() {
       // If the firstRunOnStart flag is set, subtract one interval from the start time.
       _startTime = Timekeeper::nowMillis() - (_firstRunOnStart ? _interval : 0);
-      _count = 0;
+      _passedIntervals = 0;
       _isComplete = false;
       _isCancelled = false;
       run();
     }
 
   public:
-    Timer(unsigned long interval, std::function<void(uint16_t)> callback, std::optional<uint16_t> times = 1, bool firstRunOnStart = false, bool start = true)
+    Timer(
+      // How often to run the timer.
+      unsigned long interval,
+      // What to run. The callback can take the count of times it's run so far, not counting the current run.
+      std::function<void(uint16_t)> callback,
+      // An optional number of times to run, after which it'll stop.
+      // times × interval MUST fit into ULONG_MAX.
+      // If blank, will keep running forever.
+      // NOTE: If blank, the count isn't guaranteed to be accurate.
+      std::optional<uint16_t> times = 1,
+      // Does the first run happen immediately, or after the first interval has elapsed?
+      bool firstRunOnStart = false,
+      // Start counting right away, or leave in a non-running state?
+      bool start = true,
+      // When more than one interval has elapsed, should we try to catch up by running multiple times?
+      bool catchUp = false
+    )
     :
       _interval(interval),
       _callback(callback),
       _times(times),
-      _firstRunOnStart(firstRunOnStart)
+      _firstRunOnStart(firstRunOnStart),
+      _catchUp(catchUp)
     {
       if (_interval == 0) {
         throw std::invalid_argument("Can't have a zero interval. If you want to run the timer immediately, use the firstRunOnStart flag instead.");
@@ -98,13 +116,32 @@ class Timer : public Runnable {
       if (_firstRunOnStart && _times == 1) {
         throw std::invalid_argument("Setting firstRunOnStart to true when there's only one interval makes no sense.");
       }
+      if (_times.has_value() && ULONG_MAX / _interval < _times.value()) {
+        throw std::invalid_argument("times × interval must be smaller than ULONG_MAX");
+      }
       if (start) {
         _start();
       }
     }
 
-    Timer(unsigned long interval, std::function<void()> callback, std::optional<uint16_t> times = 1, bool firstRunOnStart = false, bool start = true)
-    : Timer(interval, [callback](uint16_t _) { callback(); }, times, firstRunOnStart, start)
+    Timer(
+      // How often to run the timer.
+      unsigned long interval,
+      // What to run.
+      std::function<void()> callback,
+      // An optional number of times to run, after which it'll stop.
+      // times × interval MUST fit into ULONG_MAX.
+      // If blank, will keep running forever.
+      // NOTE: If blank, the count isn't guaranteed to be accurate.
+      std::optional<uint16_t> times = 1,
+      // Does the first run happen immediately, or after the first interval has elapsed?
+      bool firstRunOnStart = false,
+      // Start counting right away, or leave in a non-running state?
+      bool start = true,
+      // When more than one interval has elapsed, should we try to catch up by running multiple times?
+      bool catchUp = false
+    )
+    : Timer(interval, [callback](uint16_t _) { callback(); }, times, firstRunOnStart, start, catchUp)
     { }
 
     virtual void run() {
@@ -113,31 +150,40 @@ class Timer : public Runnable {
       }
 
       unsigned long now = Timekeeper::nowMillis();
-      unsigned long elapsed = now - (_startTime + _count * _interval);
+      unsigned long elapsed = now - _startTime;
       if (elapsed < _interval) {
         return;
       }
 
       // It may have been a while since it was last run;
       // more than one interval may have passed.
-      uint16_t timesToDo = elapsed / _interval;
-      if (_times.has_value()) {
-          // If we're doing it a limited number of times,
-          // and the interval > 1,
-          // don't go beyond the remaining number of times.
-          // This should work -- integer math throws away the remainder.
-        timesToDo = std::min((uint16_t)(_times.value() - _count), timesToDo);
+      uint16_t elapsedIntervals = elapsed / _interval;
+
+      if (_catchUp) {
+        uint16_t timesToDo = elapsedIntervals;
+        if (_times.has_value() && timesToDo > 1) {
+            // If we're doing it a limited number of times,
+            // don't go beyond the remaining number of times.
+          timesToDo = std::min((uint16_t)(_times.value() - _passedIntervals), timesToDo);
+        }
+
+        // Catch up by running it as many times as we need to.
+        for (uint16_t i = 0; i < timesToDo; i ++) {
+          _callback(_passedIntervals + i);
+        }
+      } else {
+        // If we're not supposed to catch up, just jump to the elapsed intervals.
+        _callback(_passedIntervals + elapsedIntervals);
       }
 
-      // Catch up by running it as many times as we need to.
-      for (uint16_t i = 0; i < timesToDo; i ++) {
-        _callback(_count);
-        _count ++;
-      }
+      _passedIntervals += elapsedIntervals;
 
-      if (_times.has_value() && _count == _times.value()) {
+      if (_times.has_value() && _passedIntervals == _times.value()) {
         _isComplete = true;
       }
+
+      // Get ready to run the next time.
+      _startTime += elapsedIntervals * _interval;
     }
 
     bool isRunning() {
@@ -156,7 +202,7 @@ class Timer : public Runnable {
 
     uint16_t getCount() {
       run();
-      return _count;
+      return _passedIntervals;
     }
 
     void cancel() {
