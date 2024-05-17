1bce80b644784479390ceb78e61b278e5117d5f4
Paul d'Aoust <amillionlobsters@gmail.com>
2024-05-17 15:25:17 -0700

    add setpoint and hysteresis struct w/ conversion to/from range

--- src/Range.h
+++ src/Range.h
@@ -7,6 +7,23 @@ struct Range {
   T max;
 
   Range(T min, T max) : min(min), max(max) { }
+
+  operator SetpointAndHysteresis() const {
+    return SetpointAndHysteresis(min + (max - min) / 2, (max - min) / 2);
+  }
+};
+
+template <typename T>
+struct SetpointAndHysteresis {
+  T setpoint;
+  // The distance that the measured value may diverge from the setpoint in either direction.
+  T hysteresis;
+
+  SetpointAndHysteresis(T setpoint, T hysteresis) : setpoint(setpoint), hysteresis(hysteresis) { }
+
+  operator Range() const {
+    return Range(setpoint - hysteresis, setpoint + hysteresis);
+  }
 };
 
 #endif
